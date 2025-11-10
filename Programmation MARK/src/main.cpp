756876b12699f88e1b89d644a47b26dc953e9f5a
leosls <saulasleo@gmail.com>
2025-11-10 11:49:25 +0100

    V1.4 (Implémentation de la fonction Virage)

--- Programmation MARK/src/main.cpp
+++ Programmation MARK/src/main.cpp
@@ -28,7 +28,7 @@ int CaptAv;
 int CaptDr;
 int CaptGa;
 int Diff;
-enum { Avancer, Obstacle, Virage }; // Définition des états possibles
+enum { Avancer, Obstacle, Virage, Securite, Arret }; // Définition des états possibles
 
 
 //---------------------------------------------------------------------------------------------------
@@ -45,75 +45,91 @@ void InitMoteurs()
     ICR5 = Vmax; // 1999 correspond a f = 4khz
     MoteurStop();
     // Interrution de débordement du timer
-    // TIMSK5 = 1 << TOIE5;
+    // TIMSK5 = 1 << TOIE5; // Pas d'interruption pour l'instant car conflit avec le moniteur série
 }
 
 //---------------------------------------------------------------------------------------------------
 // Fonction Arret Distance
 //---------------------------------------------------------------------------------------------------
-void Arretdistance() 
+void FSeccurite() 
 {
-    long DistAvant;
-    long DistDroite;
-    long DistGauche;
+    CaptAv = UltrasonicAv.MeasureInCentimeters();
+    CaptDr = UltrasonicDr.MeasureInCentimeters();
+    CaptGa = UltrasonicGa.MeasureInCentimeters();
 
-    DistAvant = UltrasonicAv.MeasureInCentimeters();
-    DistDroite = UltrasonicDr.MeasureInCentimeters();
-    DistGauche = UltrasonicGa.MeasureInCentimeters();
+	Serial.println("Mode Sécurité activé");
 
-    if (DistAvant<5 || DistGauche<5 || DistDroite<5)
-    {
-        MoteurStop();
-    }    
-}
-//---------------------------------------------------------------------------------------------------
-// Fonction Arret Moteur Bloqué
-//---------------------------------------------------------------------------------------------------
-void ArretMB()
-{
-    
+	MoteurStop();
+
 }
 
+//---------------------------------------------------------------------------------------------------
+// Fonction Virage
+//---------------------------------------------------------------------------------------------------
+void Fvirage() 
+{
+    while(CaptDr > 150 || CaptGa > 150)
+	{
+		CaptDr = UltrasonicDr.MeasureInCentimeters();
+		CaptGa = UltrasonicGa.MeasureInCentimeters();
+		
+		if (CaptDr > CaptGa)
+		{
+			// Tourner à droite
+			MoteurGD(275, 240); // TBV, ajuster les vitesses selon les tests
+			Serial.println("Virage à droite");
+		}
+		else
+		{
+			// Tourner à gauche
+			MoteurGD(240, 275); // TBV, ajuster les vitesses selon les tests
+			Serial.println("Virage à gauche");
+		}
+	}
+}
 
 //---------------------------------------------------------------------------------------------------
 // Fonction d'évitement des obstacles
 //---------------------------------------------------------------------------------------------------
-void EvitementObstacles() 
+void FevitementObstacles() 
 {
     // Code pour éviter les obstacles
     Serial.println("Evitement des obstacles activé");
 
     MoteurStop();
-    
 }
 
 //---------------------------------------------------------------------------------------------------
 // Fonction d'avancer
-//----------------------------------------------------------------------------------------------
-
+//---------------------------------------------------------------------------------------------------
 void Favancer()
 {
     CaptDr = UltrasonicDr.MeasureInCentimeters();
     CaptGa = UltrasonicGa.MeasureInCentimeters();
-    // CaptDr = (UltrasonicDrAr.MeasureInCentimeters() + UltrasonicDr.MeasureInCentimeters()) / 2;
+    // CaptDr = (UltrasonicDrAr.MeasureInCentimeters() + UltrasonicDr.MeasureInCentimeters()) / 2; // Moyenne des deux capteurs non utilisée car problème lors de la correction
     // CaptGa = (UltrasonicGaAr.MeasureInCentimeters() + UltrasonicGa.MeasureInCentimeters()) / 2;
+	
 	Serial.print("CaptGauche: ");
 	Serial.print(CaptGa);
 	Serial.print(" | CaptDroite: ");
 	Serial.println(CaptDr);
 
     Diff = CaptGa - CaptDr;
+
 	Serial.print("Diff: ");
 	Serial.println(Diff);
 
-    float Kp = 0.3;
+	// Constante proportionnelle
+    float Kp = 1; // TBV à terme (à ajuster selon les tests)
 
+	// Calcul de la correction
     float Correction = Kp * Diff;
 
 	Serial.print("Correction: ");
 	Serial.println(Correction);
 
-    float Vitesse_Droite = 250 + Correction;
+	// Calcul des vitesses des moteurs
+    float Vitesse_Droite = 250 + Correction; // TBV, vitesse de base à ajuster car lente pour l'instant 
     float Vitesse_Gauche = 250 - Correction;
 
     // Limiter les vitesses entre Vmin et Vmax
@@ -124,10 +140,10 @@ void Favancer()
 	Serial.print(Vitesse_Gauche);
 	Serial.print(" | Vitesse Droite: ");
 	Serial.println(Vitesse_Droite);
+	Serial.println("\n\n");
 
     MoteurGD(Vitesse_Gauche, Vitesse_Droite);
 
-	Serial.println("\n\n");
 }
 
 //---------------------------------------------------------------------------------------------------
@@ -141,7 +157,7 @@ void setup()
     pinMode(43,OUTPUT);
     digitalWrite(43,0);
     InitMoteurs();
-    // sei();
+    // sei(); // Pas d'interruption pour l'instant car conflit avec le moniteur série
     digitalWrite(43,1);   
 }
 
@@ -154,33 +170,53 @@ void loop()
     CaptDr = UltrasonicDr.MeasureInCentimeters();
     CaptGa = UltrasonicGa.MeasureInCentimeters();
     
+	//-----------------------------------------------------------------------------------
+	// Condition d'état
+	//-----------------------------------------------------------------------------------
     if(CaptAv < 20)
     {
         Etat = Obstacle;
     }
+	else if (CaptAv < 5 || CaptGa < 5 || CaptDr < 5)
+    {
+        Etat = Securite;
+    }
+	else if (CaptDr > 150 || CaptGa > 150)
+	{
+		Etat = Virage;
+	}
     else
     {
         Etat = Avancer;
     }
 
+	//-----------------------------------------------------------------------------------
+	// State Machine
+	//-----------------------------------------------------------------------------------
     switch (Etat)
     {
         case Avancer:
            	Favancer();
-
             break;
 
         case Obstacle:
-            EvitementObstacles();
+            FevitementObstacles();
             break;
 
 		case Virage:
-			
-			
+			Fvirage();
 			break;
         
+		case Securite:
+			
+			break;
+
+		case Arret:
+			
+			break;
+
         default:
-            //code ici
-            break;
+			MoteurStop();
+			break;
     }
 }
